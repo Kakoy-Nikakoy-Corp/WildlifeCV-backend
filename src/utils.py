8bbb191e2cf6ea3f09220ed7178777d17754d65a
Maksim Shchibrik <partycle2006nt@gmail.com>
2026-06-13 00:33:37 +0500

    make imgsz variable

--- src/utils.py
+++ src/utils.py
@@ -97,7 +97,7 @@ def calculate_letterbox_params(orig_shape: torch.Size | tuple[int, int], target_
     return LetterboxParams(r, new_w, new_h, pad_w, pad_h)
 
 
-def preprocess(img: torch.Tensor, target_size: int = 640) -> torch.Tensor:
+def preprocess(img: torch.Tensor, target_size: int = 640) -> (torch.Tensor, int, int):
     """
     Letterboxing and normalization for a raw batch of images (B, C, H, W) or a single image (C, H, W).
 
@@ -112,27 +112,31 @@ def preprocess(img: torch.Tensor, target_size: int = 640) -> torch.Tensor:
         img = img.unsqueeze(0)
 
     b, c, h, w = img.shape
-    p: LetterboxParams = calculate_letterbox_params((h, w), target_size)
+    # p: LetterboxParams = calculate_letterbox_params((h, w), target_size)
+
+    new_h = (h // 32) * 32
+    new_w = (w // 32) * 32
 
     # Resize
-    resized = f.resize(img, size=[p.new_h, p.new_w], antialias=False, interpolation=InterpolationMode.BILINEAR)
+    resized = f.resize(img, size=[new_h, new_w], antialias=False, interpolation=InterpolationMode.BILINEAR)
 
     # Create target canvas
-    canvas_dim = (b, c, target_size, target_size)
-    letterboxed = torch.full(canvas_dim, fill_value=114.0, dtype=img.dtype, device=img.device)
+    # canvas_dim = (b, c, target_size, target_size)
+    # letterboxed = torch.full(canvas_dim, fill_value=114.0, dtype=img.dtype, device=img.device)
 
-    top, bottom = p.pad_h, p.pad_h + p.new_h
-    left, right = p.pad_w, p.pad_w + p.new_w
+    # top, bottom = p.pad_h, p.pad_h + p.new_h
+    # left, right = p.pad_w, p.pad_w + p.new_w
 
     # Paste resized image in center
-    letterboxed[:, :, top:bottom, left:right] = resized
+    # letterboxed[:, :, top:bottom, left:right] = resized
 
-    return letterboxed / 255.0
+    return resized / 255.0, new_h, new_w
 
 
 def rescale_bboxes(
         bboxes: torch.Tensor,
         orig_shape: torch.Size,
+        target_shape: tuple,
         target_size: int = 640
 ) -> torch.Tensor:
     """
@@ -147,19 +151,22 @@ def rescale_bboxes(
     Returns:
         Scaled bboxes in the letterboxed (target_size, target_size) coordinate system.
     """
-    p: LetterboxParams = calculate_letterbox_params(orig_shape, target_size)
+    # p: LetterboxParams = calculate_letterbox_params(orig_shape, target_size)
 
     # Clone to avoid modifying original
     bboxes = bboxes.clone()
 
+    scale_w = orig_shape[1] / target_shape[1]
+    scale_h = orig_shape[0] / target_shape[0]
+
     # Remove padding
-    bboxes[..., 0] -= p.pad_w  # x1
-    bboxes[..., 1] -= p.pad_h  # y1
-    bboxes[..., 2] -= p.pad_w  # x2
-    bboxes[..., 3] -= p.pad_h  # y2
+    bboxes[..., 0] *= scale_w  # x1
+    bboxes[..., 1] *= scale_h  # y1
+    bboxes[..., 2] *= scale_w  # x2
+    bboxes[..., 3] *= scale_h  # y2
 
     # Scale back to original size
-    bboxes /= p.r
+    # bboxes /= p.r
 
     return bboxes
 
